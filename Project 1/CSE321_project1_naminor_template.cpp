9e48aa50ff322a27ac1b09ddc2a31fdf5d96e1db
Nick Minor <naminor@buffalo.edu>
2021-10-04 09:39:05 -0400

    Added main to template

--- Project 1/CSE321_project1_naminor_template.cpp
+++ Project 1/CSE321_project1_naminor_template.cpp
@@ -15,3 +15,13 @@
 *   Sources:        
 *
 *------------------------------------------------------------------------------------*/
+
+#include "mbed.h"
+
+// main() runs in its own thread in the OS
+int main()
+{
+    while (true) {
+
+    }
+}
