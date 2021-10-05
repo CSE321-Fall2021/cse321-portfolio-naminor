3e920f31568d205b59953feb1a6840f435b7545f
Nick <betweenthepipes66@yahoo.com>
2021-10-04 23:11:23 -0400

    Changed tab size from 2 to 4

--- Project 1/CSE321_project1_naminor_corrected_code.cpp
+++ Project 1/CSE321_project1_naminor_corrected_code.cpp
@@ -24,89 +24,89 @@
 #include "mbed.h"
 
 // Create a thread to drive an LED to have an on time of 2000ms and off time of 500ms
-Thread controller;    // Allows for scheduling and controlling of parallel tasks
+Thread controller;		// Allows for scheduling and controlling of parallel tasks
 
-void ISR_Handler();   // The Interrupt Service Routine
-void releaseButton(); // Callback function for rising edge (Button is up)
-void pushButton();    // Callback function for falling edge (Button is down)
+void ISR_Handler();		// The Interrupt Service Routine
+void releaseButton();	// Callback function for rising edge (Button is up)
+void pushButton();		// Callback function for falling edge (Button is down)
 
-DigitalOut light(LED2);     // establish blue led as an output
-InterruptIn button(BUTTON1); // Button on Nucleo is the input
-int buttonState = 0;  // Button is down when 0, and up when 1
-int lightStatus = 0;  // Light is on when 0, and off when 1
+DigitalOut light(LED2);			// establish blue led as an output
+InterruptIn button(BUTTON1);	// Button on Nucleo is the input
+int buttonState = 0;	// Button is down when 0, and up when 1
+int lightStatus = 0;	// Light is on when 0, and off when 1
 
 
 int main() {
-  // start the allowed execution of the thread
-  printf("----------------START----------------\n");
+	// start the allowed execution of the thread
+	printf("----------------START----------------\n");
 	printf("Starting state of thread: %d\n", controller.get_state());
 
-  controller.start(ISR_Handler);  // Starts the thread "controller" executing ISR_Handler()
+	controller.start(ISR_Handler);	// Starts the thread "controller" executing ISR_Handler()
 	printf("State of thread right after start: %d\n", controller.get_state());
 
-  button.rise(releaseButton);  // Calls releaseButton() on a rising edge (default)
-	button.fall(pushButton);   // Calls pushButton() on a falling edge (pressing the button)
+	button.rise(releaseButton);	// Calls releaseButton() on a rising edge (default)
+	button.fall(pushButton);	// Calls pushButton() on a falling edge (pressing the button)
 
-  return 0;
+	return 0;
 }
 
 /*
-  void ISR_Handler()
-  	Parameters: 	none
-	Return Value:	void
+	void ISR_Handler()
+	  Parameters: 	none
+	  Return Value:	void
 
-  Description:
-    Executed by the thread "controller". When lightStatus is 0, this function causes the blue light 
-    to turn on for 2000ms and then turn off for 500ms. This continues while lightStatus is 0, 
-    resulting in the blinking effect to occur. If lightStatus is 1, nothing occurs, leaving the 
-    output "light" in the last state it was in, the low state (LED2 is off).
+  	Description:
+	  Executed by the thread "controller". When lightStatus is 0, this function causes the blue light 
+	  to turn on for 2000ms and then turn off for 500ms. This continues while lightStatus is 0, 
+	  resulting in the blinking effect to occur. If lightStatus is 1, nothing occurs, leaving the 
+	  output "light" in the last state it was in, the low state (LED2 is off).
 */
 // make the handler
 void ISR_Handler() {
-  while (true) {
-    if (lightStatus == 0){
-      light = !light; // Flip the state of light from low to high
-      printf("Turned the light on, light = %d\n", light.read()); //you do need to update the print statement to be correct
-      thread_sleep_for(2000); //Thread_sleep is a time delay function, causes a 2000 unit delay
+	while (true) {
+		if (lightStatus == 0){
+			light = !light; // Flip the state of light from low to high
+			printf("Turned the light on, light = %d\n", light.read()); //you do need to update the print statement to be correct
+			thread_sleep_for(2000); //Thread_sleep is a time delay function, causes a 2000 unit delay
 
-      light = !light; // Flip the state of the light from high to low
-      printf("Turned the light off, light = %d\n", light.read());
-      thread_sleep_for(500); //Thread_sleep is a time delay function, causes a 500 unit delay
-    }
-  }
+			light = !light; // Flip the state of the light from high to low
+			printf("Turned the light off, light = %d\n", light.read());
+			thread_sleep_for(500); //Thread_sleep is a time delay function, causes a 500 unit delay
+		}
+  	}
 }
 
 /*
-  void releaseButton()
-    Parameters:   none
-    Return Value:	void
+	void releaseButton()
+	  Parameters:   none
+	  Return Value:	void
 
-  Description:
-    This function activates every time the button is released from a down state. It resets the
-    buttonState to 1 (up) so that when the button is pressed down again, the condition within 
-    pushButton() will be satisfied, which toggles the lightStatus.
+	Description:
+	  This function activates every time the button is released from a down state. It resets the
+	  buttonState to 1 (up) so that when the button is pressed down again, the condition within 
+	  pushButton() will be satisfied, which toggles the lightStatus.
 */
-void releaseButton() { // Toggle the state of the thread
-	buttonState = 1;    // Button is released, now up
+void releaseButton() {	// Toggle the state of the thread
+	buttonState = 1;	// Button is released, now up
 }
 
 /*
-  void pushButton()
-  	Parameters: 	none
-	Return Value:	void
+	void pushButton()
+  	  Parameters:	none
+	  Return Value:	void
 
-  Description:
-    After pressing the button once, this function causes lightStatus to become 1, which ends the 
-    condition in ISR_Handler() from executing during the while loop, turning the light off as 
-    a result. buttonState is set to 0 so this function does nothing until the thread is reactivated
-    using releaseButton(), which executes when the button is released.
-    Upon pressing the button again, this function resets lightStatus to 0, which satisfies the condition
-    in ISR_Handler(), causing the light to blink once more.
+	Description:
+	  After pressing the button once, this function causes lightStatus to become 1, which ends the 
+	  condition in ISR_Handler() from executing during the while loop, turning the light off as 
+	  a result. buttonState is set to 0 so this function does nothing until the thread is reactivated
+	  using releaseButton(), which executes when the button is released.
+	  Upon pressing the button again, this function resets lightStatus to 0, which satisfies the condition
+	  in ISR_Handler(), causing the light to blink once more.
 */
 void pushButton() {
-  if (buttonState == 1){  // If the button is up
-    lightStatus++;      // lightStatus increments from 0 to 1 or from 1 to 2
-    lightStatus %= 2;   // If lightStatus is 1, it stays 1. If 0, it stays 0. If it is 2, it becomes 0.
-    buttonState = 0;    // Button is pushed down
-  }
+	if (buttonState == 1){	// If the button is up
+		lightStatus++;		// lightStatus increments from 0 to 1 or from 1 to 2
+		lightStatus %= 2;	// If lightStatus is 1, it stays 1. If 0, it stays 0. If it is 2, it becomes 0.
+		buttonState = 0;	// Button is pushed down
+	}
 }
